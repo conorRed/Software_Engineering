e2628db536ec7902c096f0b2b20aed8b592ccfdd
Conor Redington <conorredington@gmail.com>
2017-03-13 16:35:01 +0000

    dear Alan, your welcome, From Conor

--- client/lib/Router.js
+++ client/lib/Router.js
@@ -1,12 +1,25 @@
 Router.configure({
-	layoutTemplate: "main"
+	layoutTemplate: "home"
 });
 
 Router.route('/',function () {
-	this.render('home');
+	this.render('home',{to: "mainSection"});
+	this.layout("main");
+});
+
+Router.route('/aboutus',function () {
+	this.render('aboutus');
 	this.layout("main");
 });
 Router.route('/dashboard',function () {
 	this.render('dashboard', {to: "mainSection"});
+	this.layout("main");
+});
+Router.route('/signup',function () {
+	this.render('sign_up_form', {to: "mainSection"});
+	this.layout("main");
+});
+Router.route('/login',function () {
+	this.render('login', {to: "mainSection"});
 	this.layout("main");
 });
